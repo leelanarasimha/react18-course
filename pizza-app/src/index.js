647f969f3eb8ae2cfa3d05684be1c699b83edd51
leelanarasimha <leela.narsimha@gmail.com>
2023-08-25 23:09:44 +0530

    Initial Commit pizza app

---
+++ pizza-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Hello React!!!!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
