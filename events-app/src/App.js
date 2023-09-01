39cf474c1de0410b34407a6e039c7d1b2202aa76
leelanarasimha <leela.narsimha@gmail.com>
2023-09-01 09:34:19 +0530

    event listeneres

--- events-app/src/App.js
+++ events-app/src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+
+const messages = ['Learn React', 'Apply for new Jobs', 'Earn new income'];
 export default function App() {
+  const step = 2;
+
+  function next() {
+    alert('next');
+  }
+
+  function previous() {
+    alert('previous');
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
-      <p className="message">Hello world</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
 
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button
+          onClick={() => previous()}
+          onMouseEnter={() => alert('text')}
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+        >
+          Previous
+        </button>
+        <button onClick={next} style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
       </div>
     </div>
   );
