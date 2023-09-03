dc78cac1718ced819dd40ef471ae80d92f35ce76
leelanarasimha <leela.narsimha@gmail.com>
2023-09-03 11:50:48 +0530

    controlled elements

--- grocery-list/src/components/AddItem.js
+++ grocery-list/src/components/AddItem.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+
 export default function AddItem() {
+  const [name, setName] = useState('Milk Packet');
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+
   return (
     <div>
       <h3>Add Item</h3>
       <form className="add-item">
         <div>
           <label>Item Name</label>
-          <input type="text" />
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div>
           <label>Quantity:</label>
-          <select>
+          <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -17,7 +23,7 @@ export default function AddItem() {
         </div>
         <div>
           <label>Price</label>
-          <input type="number" />
+          <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
         </div>
 
         <div>
