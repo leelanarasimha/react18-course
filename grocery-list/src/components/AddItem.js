83bf01140541cf58be78d0fdaffcae252534077c
leelanarasimha <leela.narsimha@gmail.com>
2023-09-03 12:08:48 +0530

    form submit

--- grocery-list/src/components/AddItem.js
+++ grocery-list/src/components/AddItem.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 
 export default function AddItem() {
-  const [name, setName] = useState('Milk Packet');
+  const [name, setName] = useState('');
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(0);
+
+  function onAddItemSubmit(e) {
+    e.preventDefault();
+    if (name === '') return;
+    const item = { name, quantity, price, id: Date.now() };
+    console.log(item);
+    setName('');
+    setQuantity(1);
+    setPrice(0);
+  }
 
   return (
     <div>
       <h3>Add Item</h3>
-      <form className="add-item">
+      <form className="add-item" onSubmit={onAddItemSubmit}>
         <div>
           <label>Item Name</label>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
@@ -16,9 +26,11 @@ export default function AddItem() {
         <div>
           <label>Quantity:</label>
           <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
+            {Array.from({ length: 20 }, (_, i) => i + 1).map((value) => (
+              <option value={value} key={value}>
+                {value}
+              </option>
+            ))}
           </select>
         </div>
         <div>
