61d6be98297460ed0e091cc3d2673ae623a41c18
leelanarasimha <leela.narsimha@gmail.com>
2023-09-30 23:49:18 +0530

    toggle list box

--- booksdb/src/components/ListBox.js
+++ booksdb/src/components/ListBox.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import BooksList from './BooksList';
 
 export default function ListBox({ booksData }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
-      <BooksList booksData={booksData} />
+      <div style={{ textAlign: 'end' }}>
+        <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+          {isOpen ? '-' : '+'}
+        </button>
+      </div>
+      {isOpen && <BooksList booksData={booksData} />}
     </div>
   );
 }
--- booksdb/src/components/StudiedList.js
+++ booksdb/src/components/StudiedList.js
@@ -1,11 +1,22 @@
 import BooksReadSummary from './BooksReadSummary';
 import BooksReadList from './BooksReadList';
+import { useState } from 'react';
 
 export default function StudiedList({ booksRead }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
-      <BooksReadSummary />
-      <BooksReadList booksRead={booksRead} />
+      <div style={{ textAlign: 'end', backgroundColor: '#eee' }}>
+        <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+          {isOpen ? '-' : '+'}
+        </button>
+      </div>
+      {isOpen && (
+        <>
+          <BooksReadSummary />
+          <BooksReadList booksRead={booksRead} />
+        </>
+      )}
     </div>
   );
 }
