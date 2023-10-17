ea8f4760b0deae7ae93529d0d50160351800334a
leelanarasimha <leela.narsimha@gmail.com>
2023-10-17 13:06:55 +0530

    mousehover on stars

--- booksdb/src/components/StarRating/StarRating.js
+++ booksdb/src/components/StarRating/StarRating.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import Star from './Star';
 
 export default function StarRating() {
-  const [rating, setRating] = useState();
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -10,9 +11,15 @@ export default function StarRating() {
   return (
     <div className="d-flex align-items-center">
       {Array.from({ length: 5 }, (k, i) => (
-        <Star key={i} onRate={() => handleRating(i + 1)} />
+        <Star
+          key={i}
+          full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+          onRate={() => handleRating(i + 1)}
+          onHoverIn={() => setTempRating(i + 1)}
+          onHoverOut={() => setTempRating(0)}
+        />
       ))}
-      <div style={{ fontSize: '20px' }}>{rating || ''}</div>
+      <div style={{ fontSize: '20px' }}>{tempRating || rating || ''}</div>
     </div>
   );
 }
