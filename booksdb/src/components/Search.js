aabbba3a2fa6b08fc81d23009c6c9ae21b704416
leelanarasimha <leela.narsimha@gmail.com>
2023-10-21 21:50:43 +0530

    ref useref hook

--- booksdb/src/components/Search.js
+++ booksdb/src/components/Search.js
@@ -1,9 +1,32 @@
+import { useEffect, useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === 'Enter') {
+        const activeElement = document.activeElement;
+        inputRef.current.focus();
+        if (activeElement !== inputRef.current) {
+          setQuery('');
+        }
+      }
+    }
+    document.addEventListener('keydown', callback);
+    inputRef.current.focus();
+    return () => {
+      document.removeEventListener('keydown', callback);
+    };
+  }, []);
+
   return (
     <input
+      className="search"
       type="text"
       placeholder="Search Books..."
       value={query}
+      ref={inputRef}
       onInput={(e) => setQuery(e.target.value)}
     />
   );
