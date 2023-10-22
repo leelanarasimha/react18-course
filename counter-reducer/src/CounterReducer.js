f1fb8f38aadd3d6c8216f564ed1fa63ef7506135
leelanarasimha <leela.narsimha@gmail.com>
2023-10-22 22:47:14 +0530

    usereducer hook

--- counter-reducer/src/CounterReducer.js
+++ counter-reducer/src/CounterReducer.js
@@ -1,19 +1,23 @@
 import { useReducer } from 'react';
 
+const initialState = { count: 0, step: 1 };
 function reducer(state, action) {
+  console.log(state, action);
   switch (action.type) {
     case 'inc':
-      return state + 1;
+      return { ...state, count: state.count + state.step };
     case 'dec':
-      return state - 1;
-    case 'reset':
-      return 0;
+      return { ...state, count: state.count - state.step };
     case 'setCount':
-      return action.payload;
+      return { ...state, count: action.payload };
+    case 'setStep':
+      return { ...state, step: action.payload };
+    case 'reset':
+      return initialState;
   }
 }
 export default function CounterReducer() {
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   function inc() {
     dispatch({ type: 'inc' });
@@ -31,11 +35,24 @@ export default function CounterReducer() {
     dispatch({ type: 'setCount', payload: +e.target.value });
   }
 
+  function setStep(e) {
+    dispatch({ type: 'setStep', payload: +e.target.value });
+  }
+
   return (
     <div>
+      <div>Step</div>
+      <div>
+        <select value={state.step} onChange={setStep}>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+        </select>
+      </div>
       <div>Counter</div>
       <div>
-        <input value={count} onChange={updateCount} />
+        <input value={state.count} onChange={updateCount} />
       </div>
       <div>
         <button onClick={inc}>+</button>&nbsp;
