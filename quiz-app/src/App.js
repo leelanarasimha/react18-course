507c5f088642dee5d35bd1535bb7f737c55041db
leelanarasimha <leela.narsimha@gmail.com>
2023-10-23 12:53:01 +0530

    new Quiz app

---
+++ quiz-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz-app/src/components/Header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1 className="header">Quiz App</h1>;
+}
