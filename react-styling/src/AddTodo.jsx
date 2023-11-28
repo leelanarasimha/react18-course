1bf74015d814e5bfe3e9683110485c17bc0073f8
leelanarasimha <leela.narsimha@gmail.com>
2023-11-28 10:02:02 +0530

    inline styles

--- react-styling/src/AddTodo.jsx
+++ react-styling/src/AddTodo.jsx
@@ -1,12 +1,13 @@
+import { styles } from './styles';
 export default function AddTodo() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <h2 style={{ padding: '10px 20px', textAlign: 'center', color: 'white' }}>TODO</h2>
+    <div style={styles.Container}>
+      <h2 style={styles.Header}>TODO</h2>
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <label style={{ padding: '10px 20px', textAlign: 'center' }}>What needs to be Done?</label>
+      <div style={styles.LabelContainer}>
+        <label style={styles.Label}>What needs to be Done?</label>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={styles.FormContainer}>
         <form>
           <input type="text" />
           <button>Add</button>
---
+++ react-styling/src/styles.js
@@ -0,0 +1,14 @@
+const spacing = '10px 20px';
+const Container = { display: 'flex', flexDirection: 'column' };
+const Header = { padding: spacing, textAlign: 'center', color: 'white' };
+const LabelContainer = { display: 'flex', justifyContent: 'center', alignItems: 'center' };
+const Label = { padding: spacing, textAlign: 'center' };
+const FormContainer = { display: 'flex', justifyContent: 'center', alignItems: 'center' };
+
+export const styles = {
+  Container,
+  Header,
+  LabelContainer,
+  Label,
+  FormContainer
+};
