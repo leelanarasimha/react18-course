916e097216b9473c987c6a08e2f07b204a5ce85a
leelanarasimha <leela.narsimha@gmail.com>
2023-12-03 22:08:07 +0530

    cities list useeffect

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Product from './pages/Product';
 import About from './pages/About';
 import PageNotFound from './pages/PageNotFound';
-import PageNav from './components/PageNav';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CitiesList from './pages/CitiesList';
 
 function App() {
+  const [cities, setCities] = useState('');
+  const [loading, setLoading] = useState(false);
+  const citiesUrl = 'http://localhost:9000/cities';
+  async function fetchCities() {
+    try {
+      setLoading(true);
+      let response = await fetch(citiesUrl);
+      let cities = await response.json();
+      setCities(cities);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(function () {
+    fetchCities();
+  });
+
   return (
     <div>
       <BrowserRouter>
@@ -18,8 +38,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<div>Cities list</div>} />
-            <Route path="cities" element={<div>Cities List</div>} />
+            <Route index element={<CitiesList cities={cities} loading={loading} />} />
+            <Route path="cities" element={<CitiesList cities={cities} loading={loading} />} />
             <Route path="countries" element={<div>Cuntries list</div>} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
---
+++ travel-app/src/pages/CitiesList.jsx
@@ -0,0 +1,4 @@
+export default function CitiesList({ cities, loading }) {
+  console.log(cities);
+  return <div>Cities Listdssd</div>;
+}
